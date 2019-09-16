1a64b436a0b91f7828b94e44f22a11eaae7123ad
hmchuong <minhchuong.itus@gmail.com>
2019-09-16 10:08:17 +0700

    debug

--- models.py
+++ models.py
@@ -187,9 +187,10 @@ class YOLOLayer(nn.Module):
                 anchors=self.scaled_anchors,
                 ignore_thres=self.ignore_thres,
             )
-            return None
+            
             # Loss : Mask outputs to ignore non-existing objects (except with conf. loss)
             loss_x = self.mse_loss(x[obj_mask], tx[obj_mask])
+            return None
             loss_y = self.mse_loss(y[obj_mask], ty[obj_mask])
             loss_w = self.mse_loss(w[obj_mask], tw[obj_mask])
             loss_h = self.mse_loss(h[obj_mask], th[obj_mask])
